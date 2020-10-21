1b273da91f56de90fce1694af43267f09d05a003
fabriciominowa <53982421+fabriciominowa@users.noreply.github.com>
2020-10-20 21:13:28 -0300

    jogo

---
+++ Assets/Movimentação.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimentação : MonoBehaviour
+{
+    public float moveSpeed = 20.0f;
+    private Vector3 Pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Pos = transform.position;
+        Pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        Pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.position = Pos;
+    }
+}
---
+++ Assets/menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class menu : MonoBehaviour
+{
+    public bool Play;
+    public bool Exit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("jogo");
+            Debug.Log("apertou");
+        }
+    }
+
+   
+}
+
